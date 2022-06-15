5338fbcfa77a3a2f4e13ba3e87c7138c623fa11d
Luis Sotomayor Moya <luis.sotomayor.moya@gmail.com>
2022-06-15 17:18:44 -0500

    Sript de la base da datos y datos de la tabla completos para su importación

--- TareasAppWeb/TareasWebApp-db/tareas-DB.sql
+++ TareasAppWeb/TareasWebApp-db/tareas-DB.sql
@@ -1,18 +1,61 @@
--- Creación de la base de datos 'tareas-DB' --
-CREATE SCHEMA `tareas-DB` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+CREATE DATABASE  IF NOT EXISTS `tareas-DB` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8_spanish_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `tareas-DB`;
+-- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+--
+-- Host: localhost    Database: tareas-DB
+-- ------------------------------------------------------
+-- Server version	8.0.29-0ubuntu0.22.04.2
 
--- Creación de la tabla 'tarea' --
-CREATE TABLE `tareas-DB`.`tarea` (
-  `idtarea` INT NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(255) NULL,
-  `descripcion` LONGTEXT NULL,
-  `fechacreacion` DATETIME NULL,
-  `fechaactualizacion` DATETIME NULL,
-  `fechaeliminacion` DATETIME NULL,
-  `estado` CHAR(1) NULL,
-  `eliminado` CHAR(1) NULL,
-  `secuencia` INT NULL,
-  `subtarea` INT NULL,
-  PRIMARY KEY (`idtarea`));
-  
-  
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tarea`
+--
+
+DROP TABLE IF EXISTS `tarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tarea` (
+  `idtarea` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `descripcion` longtext COLLATE utf8_spanish_ci,
+  `fechacreacion` datetime DEFAULT NULL,
+  `fechaactualizacion` datetime DEFAULT NULL,
+  `fechaeliminacion` datetime DEFAULT NULL,
+  `estado` char(1) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `eliminado` char(1) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `secuencia` int DEFAULT NULL,
+  `subtarea` int DEFAULT NULL,
+  PRIMARY KEY (`idtarea`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tarea`
+--
+
+LOCK TABLES `tarea` WRITE;
+/*!40000 ALTER TABLE `tarea` DISABLE KEYS */;
+INSERT INTO `tarea` VALUES (1,'Prueba 1 - Título','Prueba 1 - Descripción','2022-06-13 14:40:59','2022-06-13 14:40:59','2022-06-13 14:40:59','P','0',1,0),(2,'Prueba 2 - Título','Prueba 2 - Descripción','2022-06-13 14:40:59','2022-06-13 14:40:59','2022-06-13 14:40:59','P','0',2,0);
+/*!40000 ALTER TABLE `tarea` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-15 17:16:21
